23037cd2af8ea516ce28677d5f898ead54c0cfd1
王莹 <jinzhenzhong@wangyingdeMacBook-Pro.local>
2015-06-24 00:04:57 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by 王莹 on 15/6/24.
+//  Copyright (c) 2015年 超级中. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by 王莹 on 15/6/24.
+//  Copyright (c) 2015年 超级中. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
